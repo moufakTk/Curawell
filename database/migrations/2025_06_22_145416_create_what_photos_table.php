fcff66c2000462d43501a3e5c4f1d8122e69787d
mohnd <mohndmsre6789@gmail.com>
2025-06-24 22:54:43 +0300

    تزبيط اسماء الجداول والموديلات

--- database/migrations/2025_06_22_145416_create_what_photos_table.php
+++ database/migrations/2025_06_22_145416_create_what_photos_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('what_photos', function (Blueprint $table) {
             $table->id();
             $table->foreignId('skiagraph_order_id')->constrained('skiagraph_orders')->cascadeOnDelete();
-            $table->foreignId('small_servise_id')->constrained('small_servises')->cascadeOnDelete();
+            $table->foreignId('small_service_id')->constrained('small_services')->cascadeOnDelete();
             $table->decimal('price');
             $table->timestamps();
         });
